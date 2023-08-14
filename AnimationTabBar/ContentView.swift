c4239c8c4015bd72e1a251ebb4d425ab3712f6ad
eyhdev <alaqsa335@gmail.com>
2023-08-14 19:52:36 +0300

    TabBar Animation

--- AnimationTabBar/ContentView.swift
+++ AnimationTabBar/ContentView.swift
@@ -8,19 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var currentTab: Tab = .House
+    @Namespace var animation
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        TabBar()
+    }
+    @ViewBuilder
+    func TabBar()-> some View{
+        HStack(spacing: 0){
+            ForEach(Tab.allCases, id: \.rawValue) { tab in
+                VStack(spacing: -2){
+                    Image(tab.rawValue)
+                        .renderingMode(.template)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 28, height: 29)
+                        .foregroundColor(currentTab == tab ? .white : .gray.opacity(0.6))
+                    
+                    if currentTab == tab{
+                        Circle()
+                            .fill(.white)
+                            .frame(width: 5, height: 5)
+                            .offset(y: 10)
+                            .matchedGeometryEffect(id: "Tab", in: animation)
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .contentShape(Rectangle())
+                .onTapGesture {
+                    withAnimation(.easeInOut){currentTab = tab}
+                }
+            }
         }
-        .padding()
+        .padding(.horizontal)
+        .padding(.bottom, 10)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
     }
 }
---
+++ AnimationTabBar/Tab.swift
@@ -0,0 +1,15 @@
+//
+//  Tab.swift
+//  AnimationTabBar
+//
+//  Created by eyh.mac on 14.08.2023.
+//
+
+import SwiftUI
+
+enum Tab: String, CaseIterable {
+    case House = "house"
+    case Explore = "explore"
+    case Person = "person"
+}
+
