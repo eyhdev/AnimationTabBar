d73468c916ba7659f92fbd567a6b3a1ab055af24
eyhdev <alaqsa335@gmail.com>
2023-08-14 14:13:34 +0300

    Initial Commit

---
+++ AnimationTabBar/AnimationTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationTabBarApp.swift
+//  AnimationTabBar
+//
+//  Created by eyh.mac on 14.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct AnimationTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
